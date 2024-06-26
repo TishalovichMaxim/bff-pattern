5b7b11d4a983f531f03dde7a5b03a0e3d8d9753c
Tishalovich Maxim <tishalovichm@gmail.com>
2024-06-26 11:14:55 +0300

    Init mobile bff spring boot app

---
+++ mobilebff/src/test/java/by/tishalovichm/mobilebff/MobilebffApplicationTests.java
@@ -0,0 +1,13 @@
+package by.tishalovichm.mobilebff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilebffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
